f1bf040907a9190e243199c10515556078292725
JoseVelazcoH <velazco.joseh@gmail.com>
2025-12-30 18:02:24 -0600

    feat: add custom types and preprocessing utilities for GWOSC data

---
+++ constants/gw_constants.py
@@ -0,0 +1,2 @@
+cm2kpc: float = 3.24078e-22
+default_kpc_distance: float = 10.0
---
+++ core/types/__init__.py
@@ -0,0 +1,15 @@
+from .custom_types import (
+    LoaderData,
+    DetectorData,
+    GWOSCFileData,
+    ProcessedNoiseData,
+    WindowedSample,
+    TransformerData,
+    ExporterData
+    )
+
+__all__ = [
+            'LoaderData', 'DetectorData', 'GWOSCFileData',
+            'ProcessedNoiseData', 'WindowedSample', 'TransformerData',
+            'ExporterData'
+        ]
